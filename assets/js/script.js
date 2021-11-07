e4b53d6bf1662c7eb879e2f989e77e8260efe068
liamsmith3194 <smith.liam1994@gmail.com>
2021-11-07 10:23:29 +0000

    random maths numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+/**
+ * The main game "loop" called dskf udgh fhgfff sf ggf
+ * i hgdfh gfhgdfhg h d 
+ */
+
 function runGame() {
 
-}
+    let num1 = Math.floor(Math.random() *25)+1;
+    let mum2 = Math.floor(Math.random() *25)+1;
 
+}
 
 function checkAnswer() {
     
